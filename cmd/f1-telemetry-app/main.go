1bcb5bf0447d220e0bd2f984e1ffbcc1d61d70be
Bengisu Akkir <bengisuakkir@gmail.com>
2022-10-13 18:27:14 +0200

    added telemetry values

--- cmd/f1-telemetry-app/main.go
+++ cmd/f1-telemetry-app/main.go
@@ -1,42 +1,78 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
-	"math"
-	"net"
+	"github.com/anilmisirlioglu/f1-telemetry-go/pkg/env/event"
+	"github.com/anilmisirlioglu/f1-telemetry-go/pkg/packets"
+	"github.com/anilmisirlioglu/f1-telemetry-go/pkg/telemetry"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
 )
 
-func floatFromBytes(bytes []byte) float32 {
-	bits := binary.LittleEndian.Uint32(bytes)
-	float := math.Float32frombits(bits)
-	return float
-}
-
-func bits32FromBuf(buf []byte, i int) []byte {
-	return buf[4*i : 4*(i+1)]
-}
-
 func main() {
-
-	sAddr, err := net.ResolveUDPAddr("udp", "0.0.0.0:20777")
-	if err != nil {
-		fmt.Errorf("Error: ", err)
-	}
-
-	sConn, err := net.ListenUDP("udp", sAddr)
-	if err != nil {
-		fmt.Errorf("Error: ", err)
-	}
-	defer sConn.Close()
-
-	buf := make([]byte, 1289)
-
-	for {
-		_, _, err := sConn.ReadFromUDP(buf)
-		fmt.Println("Received Speed :", floatFromBytes(bits32FromBuf(buf, 7))*3.6)
+	// prometheus handler
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		err := http.ListenAndServe(":8080", nil)
 		if err != nil {
-			fmt.Errorf("Error: ", err)
+			log.Fatal(err)
 		}
+	}()
+
+	client, err := telemetry.NewClientByCustomIpAddressAndPort("0.0.0.0", 20777)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	client.OnEventPacket(func(packet *packets.PacketEventData) {
+		switch packet.EventCodeString() {
+		case event.SpeedTrapTriggered:
+			trap := packet.EventDetails.(*packets.SpeedTrap)
+			if trap.VehicleIdx == packet.Header.PlayerCarIndex {
+				fmt.Printf("Speed Trap: %f\n\n", trap.Speed)
+				speedTrapMetric.Set(float64(trap.Speed))
+			}
+			break
+		case event.FastestLap:
+			fp := packet.EventDetails.(*packets.FastestLap)
+			if fp.VehicleIdx == packet.Header.PlayerCarIndex {
+				fmt.Printf("Fastest Lap: %f seconds\n", fp.LapTime)
+				fastestLapMetric.Set(float64(fp.LapTime))
+			}
+			break
+		}
+	})
+
+	client.OnCarTelemetryPacket(func(packet *packets.PacketCarTelemetryData) {
+		car := packet.CarTelemetryData[packet.Header.PlayerCarIndex]
+		speed := float64(car.Speed)
+		engineRPM := float64(car.EngineRPM)
+		fmt.Println("Received Speed :", speed)
+		speedMetric.Set(speed)
+		engineRPMMetric.Set(engineRPM)
+
+		for i, breaks := range []string{"rl", "rr", "fl", "fr"} {
+			brakesTempMetric.WithLabelValues(breaks).Set(float64(car.BrakesTemperature[i]))
+			fmt.Println("Break temp :", float64(car.BrakesTemperature[i]))
+		}
+	})
+
+	client.OnLapPacket(func(packet *packets.PacketLapData) {
+		car := packet.LapData[packet.Header.PlayerCarIndex]
+		lastLapTime := float64(car.LastLapTime)
+		fmt.Println("Last Lap :", lastLapTime)
+		lastLapTimeMetric.Set(lastLapTime)
+	})
+
+	client.OnCarStatusPacket(func(packet *packets.PacketCarStatusData) {
+		carStatus := packet.CarStatusData[packet.Header.PlayerCarIndex]
+		tyresAgeLapsMetric.Set(float64(carStatus.TyresAgeLaps))
+
+		for i, trye := range []string{"rl", "rr", "fl", "fr"} {
+			tyreWearMetric.WithLabelValues(trye).Set(float64(carStatus.TyresWear[i]))
+		}
+	})
+
+	client.Run()
 }
